a82ebfc13c9f9084eaaa033897bb5416c79cf713
Claudio <lapertosa.c@gmail.com>
2022-04-16 19:20:44 +0200

    E2E tests initialized

---
+++ GamesLand.Tests.E2E/Users/Controllers/UsersControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using GamesLand.Core.Users;
+using GamesLand.Tests.Helpers;
+using GamesLand.Web.Users.Responses;
+using Xunit;
+
+namespace GamesLand.Tests.E2E.Users.Controllers;
+
+public class UsersControllerTests : IntegrationTestBase
+{
+    [Fact]
+    public async Task Sign_Up_User_With_Valid_Data()
+    {
+        User user = new User()
+        {
+            FirstName = "Name",
+            LastName = "Last name",
+            Email = "email@email.com",
+            Password = "password"
+        };
+
+        var res = await Client.PostAsJsonAsync("api/users", user);
+        UserResponse? userResponse = await res.Content.ReadFromJsonAsync<UserResponse>();
+
+        Assert.Equal(HttpStatusCode.Created, res.StatusCode);
+        Assert.Equal(user.FirstName, userResponse?.FirstName);
+        Assert.Equal(user.LastName, userResponse?.LastName);
+        Assert.Equal(user.Email, userResponse?.Email);
+    }
+
+    [Fact]
+    public async Task Sign_Up_User_With_A_Not_Valid_Email()
+    {
+        User user = new User()
+        {
+            FirstName = "Name",
+            LastName = "Last name",
+            Email = "123",
+            Password = "password"
+        };
+
+        var res = await Client.PostAsJsonAsync("api/users", user);
+
+        Assert.Equal(HttpStatusCode.BadRequest, res.StatusCode);
+    }
+
+    [Fact]
+    public async Task Sign_Up_User_Without_An_Email()
+    {
+        User user = new User()
+        {
+            FirstName = "Name",
+            LastName = "Last name",
+            Password = "password"
+        };
+
+        var res = await Client.PostAsJsonAsync("api/users", user);
+
+        Assert.Equal(HttpStatusCode.BadRequest, res.StatusCode);
+    }
+
+    [Fact]
+    public async Task Sign_Up_User_With_A_Not_Valid_Password()
+    {
+        User user = new User()
+        {
+            FirstName = "Name",
+            LastName = "Last name",
+            Email = "email@email.com",
+            Password = "pass"
+        };
+
+        var res = await Client.PostAsJsonAsync("api/users", user);
+
+        Assert.Equal(HttpStatusCode.BadRequest, res.StatusCode);
+    }
+
+    [Fact]
+    public async Task Sign_Up_User_Without_A_Password()
+    {
+        User user = new User()
+        {
+            FirstName = "Name",
+            LastName = "Last name",
+            Email = "email@email.com"
+        };
+
+        var res = await Client.PostAsJsonAsync("api/users", user);
+
+        Assert.Equal(HttpStatusCode.BadRequest, res.StatusCode);
+    }
+}
---
+++ GamesLand.Tests.Helpers/IntegrationTestBase.cs
@@ -0,0 +1,10 @@
+﻿namespace GamesLand.Tests.Helpers;
+
+public class TestBase : IntegrationTestHelper, IDisposable
+{
+    public async void Dispose()
+    {
+        Client.Dispose();
+        await DropTables();
+    }
+}
